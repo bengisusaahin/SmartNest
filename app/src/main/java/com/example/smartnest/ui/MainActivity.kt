33c453e3b0364af17aa039fa7e5748b4b680ba0a
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2025-01-23 23:34:08 +0300

    feat: Added second fragment to navigation

--- app/src/main/java/com/example/smartnest/ui/MainActivity.kt
+++ app/src/main/java/com/example/smartnest/ui/MainActivity.kt
@@ -3,12 +3,9 @@ package com.example.smartnest.ui
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import com.example.smartnest.R
 import com.example.smartnest.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
