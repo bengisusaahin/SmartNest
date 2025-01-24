0784f4c29eaca55ae64c0bb5efa273d8f28a02d9
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2025-01-24 19:18:27 +0300

    style: Added light fragment

--- app/src/main/java/com/example/smartnest/ui/light/LightFragment.kt
+++ app/src/main/java/com/example/smartnest/ui/light/LightFragment.kt
@@ -1,20 +1,27 @@
 package com.example.smartnest.ui.light
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
 import com.example.smartnest.R
+import com.example.smartnest.databinding.FragmentLightBinding
+import com.example.smartnest.ui.BaseFragment
 
-class LightFragment : Fragment() {
+class LightFragment : BaseFragment<FragmentLightBinding>(FragmentLightBinding::inflate) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_light, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupAppBar(
+            toolbar = binding.appBar.toolbar,
+            title = getString(R.string.light),
+            showBackButton = true,
+            navigationIcon = R.drawable.outline_arrow_back_24,
+            rightIcon = R.drawable.outline_highlight_24, // Sağdaki light ikonu
+            onRightIconClick = {
+                Toast.makeText(requireContext(), "Light icon clicked", Toast.LENGTH_SHORT).show()
+            }
+        )
     }
 
 }
